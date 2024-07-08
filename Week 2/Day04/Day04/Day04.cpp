5e86e4e431767d3e5f836db26ba1fd9e517bb5f0
Garrett Girod <girodg@hotmail.com>
2024-07-08 10:26:10 -0400

    bubble sort

--- Week 2/Day04/Day04/Day04.cpp
+++ Week 2/Day04/Day04/Day04.cpp
@@ -26,8 +26,113 @@ unsigned long factorial(unsigned int N)
     return N * factorial(N - 1);
 }
 
+/*
+    ╔═══════╗
+    ║Sorting║
+    ╚═══════╝
+
+    Sorting is used to order the items in a vector/array is a specific way
+
+    CHALLENGE 2:
+
+        Convert this BubbleSort pseudo-code into a method
+
+        procedure bubbleSort(A : list of sortable items)
+          n := length(A)
+          repeat
+              swapped := false
+              for i := 1 to n - 1 inclusive do
+                  if A[i - 1] > A[i] then
+                      swap(A, i - 1, i)
+                      swapped = true
+                  end if
+              end for
+              n := n - 1
+          while swapped
+        end procedure
+
+*/
+
+void bubbleSort(std::vector<int>& A)
+{
+    int n = A.size();//n := length(A)
+    bool swapped;
+    do //repeat
+    {
+        swapped = false;//swapped := false
+        //for i := 1 to n - 1 inclusive do
+        for (int i = 1; i <= n - 1; i++)
+        {
+            //if A[i - 1] > A[i] then
+            if (A[i - 1] > A[i])
+            {
+                //TODO: fix this later
+                //swap(A, i - 1, i);
+                //int temp = A[i - 1];
+                //A[i - 1] = A[i];
+                //A[i] = temp;
+                //OR...use "modern" c++
+                std::swap(A[i - 1], A[i]);
+
+                swapped = true;
+            }//end if
+        }//end for
+        --n;//pre-decrement        
+    } while (swapped);//while swapped
+}//end procedure
+void bubbleSort(std::vector<std::string>& A)
+{
+    int n = A.size();//n := length(A)
+    bool swapped;
+    do //repeat
+    {
+        swapped = false;//swapped := false
+        //for i := 1 to n - 1 inclusive do
+        for (int i = 1; i <= n - 1; i++)
+        {
+            //if A[i - 1] > A[i] then
+            int comparison = _stricmp(A[i - 1].c_str(), A[i].c_str());
+            //if (A[i - 1] > A[i])//case sensistive
+            if(comparison > 0)//case insensitive
+            {
+                //TODO: fix this later
+                //swap(A, i - 1, i);
+                //int temp = A[i - 1];
+                //A[i - 1] = A[i];
+                //A[i] = temp;
+                //OR...use "modern" c++
+                std::swap(A[i - 1], A[i]);
+
+                swapped = true;
+            }//end if
+        }//end for
+        --n;//pre-decrement        
+    } while (swapped);//while swapped
+}//end procedure
 int main()
 {
+    std::vector<int> nummies{ 13, 7, 0, 5 };
+    for (auto& nummie : nummies)
+        std::cout << nummie << " ";
+    std::cout << "\n";
+
+    bubbleSort(nummies);
+
+    for (auto& nummie : nummies)
+        std::cout << nummie << " ";
+    std::cout << "\n";
+
+    std::cin.get();
+
+    std::vector<std::string> names = { "Wonder Woman", "Superman", "Batman", "Flash", "Aquaman" };
+    for (auto& name : names)
+        std::cout << name << "\n";
+    //call your BubbleSort on the names vector.
+    bubbleSort(names);
+    //print the sorted vector.
+    for (auto& name : names)
+        std::cout << name << "\n";
+    std::cin.get();
     /*
         ╔═════════╗
         ║Recursion║
@@ -110,38 +215,4 @@ int main()
 
 
 
-
-    /*
-        ╔═══════╗
-        ║Sorting║
-        ╚═══════╝
-
-        Sorting is used to order the items in a vector/array is a specific way
-
-        CHALLENGE 2:
-
-            Convert this BubbleSort pseudo-code into a method
-
-            procedure bubbleSort(A : list of sortable items)
-              n := length(A)
-              repeat
-                  swapped := false
-                  for i := 1 to n - 1 inclusive do
-                      if A[i - 1] > A[i] then
-                          swap(A, i - 1, i)
-                          swapped = true
-                      end if
-                  end for
-                  n := n - 1
-              while swapped
-            end procedure
-
-    */
-    std::vector<std::string> names = { "Wonder Woman", "Superman", "Batman", "Flash", "Aquaman" };
-    //call your BubbleSort on the names vector.
-
-
-    //print the sorted vector.
-    for (auto& name : names)
-        std::cout << name << "\n";
 }
